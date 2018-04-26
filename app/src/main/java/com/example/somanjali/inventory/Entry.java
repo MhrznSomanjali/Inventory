c6ab28a1206165f542e48d0d0e8191a35bc83454
MhrznSomanjali <MhrznSomanjali@github.com>
2018-04-26 20:50:17 +0545

    adding listeners

--- app/src/main/java/com/example/somanjali/inventory/Entry.java
+++ app/src/main/java/com/example/somanjali/inventory/Entry.java
@@ -6,9 +6,12 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
-public class Entry extends android.support.v4.app.Fragment {
+public class Entry extends android.support.v4.app.Fragment implements View.OnClickListener {
+    Button cancelbutton,savebutton;
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.entry,container,false);
@@ -20,7 +23,29 @@ public class Entry extends android.support.v4.app.Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        initactivity();
+        setlisteners();
+
 
     }
+    public void initactivity(){
+        cancelbutton=getActivity().findViewById(R.id.cancelbtn);
+        savebutton=getActivity().findViewById(R.id.savebtn);
     }
+    public void setlisteners(){
+        cancelbutton.setOnClickListener(this);
+        savebutton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==cancelbutton){
+            System.exit(0);
+        }
+        else if(v==savebutton){
+            //realm
+        }
+
+    }
+}
 
